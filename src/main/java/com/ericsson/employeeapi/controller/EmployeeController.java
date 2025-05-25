d11f98157f21bb4e833148025a2f799a80a2d219
David Baranyai <17344186+davidBaranyai@users.noreply.github.com>
2025-05-25 14:30:01 +0200

    add openapi documentation

--- src/main/java/com/ericsson/employeeapi/controller/EmployeeController.java
+++ src/main/java/com/ericsson/employeeapi/controller/EmployeeController.java
@@ -5,11 +5,15 @@ import com.ericsson.employeeapi.dto.EmployeeRequestDto;
 import com.ericsson.employeeapi.dto.EmploymentDaysDto;
 import com.ericsson.employeeapi.exception.EmployeeNotFoundException;
 import com.ericsson.employeeapi.service.EmployeeService;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.FieldError;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,10 +23,18 @@ import java.util.Map;
 
 @RequiredArgsConstructor
 @RestController
-@Validated
+@OpenAPIDefinition(
+        info = @Info(
+                version = "1.0.0-SNAPSHOT",
+                title = "Employee OpenAPI Definition",
+                description = "This is an API for managing employees")
+)
+@Tag(name = "Employee API", description = "Employee related APIs")
 public class EmployeeController {
     private final EmployeeService employeeService;
 
+    @Operation(summary = "Get a list of all employee", parameters = @Parameter(name = "manager",
+            description = "ID of a manager", example = "123"))
     @GetMapping("/employees")
     @ResponseBody
     public List<EmployeeDto> getEmployees(@RequestParam(required = false, name = "manager") Integer manager) {
@@ -30,24 +42,28 @@ public class EmployeeController {
                 : employeeService.getAllEmployees();
     }
 
+    @Operation(summary = "Get the number of days the employee has in employment")
     @GetMapping("/employee/{id}/days")
     @ResponseBody
     public EmploymentDaysDto getEmploymentDays(@PathVariable(name = "id") Integer id) {
         return employeeService.getEmploymentDays(id);
     }
 
+    @Operation(summary = "Create new employee")
     @PostMapping("/employee")
     @ResponseBody
     public EmployeeDto createEmployee(@Valid @RequestBody EmployeeRequestDto request) {
         return employeeService.createEmployee(request);
     }
 
+    @Operation(summary = "Update existing employee")
     @PutMapping("/employee/{id}")
     @ResponseBody
     public EmployeeDto putEmployee(@PathVariable(name = "id") Integer id, @Valid @RequestBody EmployeeRequestDto request) {
         return employeeService.updateEmployee(id, request);
     }
 
+    @Operation(summary = "Delete existing employee")
     @DeleteMapping("/employee/{id}")
     @ResponseStatus(value = HttpStatus.NO_CONTENT)
     public void deleteEmployee(@PathVariable(name = "id") Integer id) {
